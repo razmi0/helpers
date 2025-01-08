1f79aed72047e19f296d67d6fdb06710fa809935
razmiO <thomas.cuesta31@gmail.com>
2025-01-09 00:04:55 +0100

    Refactor mockFetch to accept additional expected value "mutated"

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -1,21 +1,38 @@
 // Fetch mock
 
 // Successful response
-export const mockFetch = <T>({ returnData, expected }: { returnData: T; expected: "error" | "success" }) => {
+export const mockFetch = <T>({
+    returnData,
+    expected,
+}: {
+    returnData: T;
+    expected: "error" | "success" | "mutated";
+}) => {
     globalThis.fetch = (_input: string | URL | Request = "") => {
-        if (expected === "error") {
-            return Promise.resolve(
-                new Response(JSON.stringify(returnData), {
-                    status: 404,
-                    statusText: "Not Found",
-                })
-            );
+        switch (expected) {
+            case "mutated":
+                return Promise.resolve(
+                    new Response(JSON.stringify(returnData), {
+                        status: 200,
+                        statusText: "Success",
+                    })
+                );
+
+            case "error":
+                return Promise.resolve(
+                    new Response(JSON.stringify(returnData), {
+                        status: 404,
+                        statusText: "Not Found",
+                    })
+                );
+
+            default:
+                return Promise.resolve(
+                    new Response(JSON.stringify(returnData), {
+                        status: 200,
+                        statusText: "Success",
+                    })
+                );
         }
-        return Promise.resolve(
-            new Response(JSON.stringify(returnData), {
-                status: 200,
-                statusText: "Success",
-            })
-        );
     };
 };
